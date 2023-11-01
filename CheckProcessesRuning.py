e4d76767191c73d415a0239e94253d8ed5f87813
Patryk Bala <patrykbala89@gmail.com>
2023-11-01 18:18:44 +0100

    add support for Linux

--- CheckProcessesRuning.py
+++ CheckProcessesRuning.py
@@ -1,18 +1,38 @@
 import subprocess
+import platform
 
 def list_background_processes():
+    processes = []
     try:
-        output = subprocess.check_output("tasklist", shell=True, text=True)
-        lines = output.strip().split('\n')[3:]  # Skip the first 3 lines of the header
-        
-        print("Processes running in the background:")
-        for line in lines:
-            fields = line.split()
-            pid = fields[1]
-            name = fields[0]
-            print(f"PID: {pid}, Name: {name}")
+        operating_system = platform.system()
+
+        if operating_system == 'Windows':
+            output = subprocess.check_output("tasklist", shell=True, text=True)
+            lines = output.strip().split('\n')[3:]  # Skip the first 3 lines of the header
+            for line in lines:
+                fields = line.split()
+                pid = fields[1]
+                name = fields[0]
+                processes.append((pid, name))
+
+        elif operating_system == 'Linux':
+            output = subprocess.check_output(["ps", "-e"], text=True)
+            lines = output.strip().split('\n')[1:]  # Skip the header
+            for line in lines:
+                fields = line.split()
+                pid = fields[0]
+                name = fields[-1]
+                processes.append((pid, name))
+        else:
+            print("Unsupported operating system.")
+
     except subprocess.CalledProcessError as e:
         print("Error. Unable to get process list:", e)
 
+    return processes
+
 if __name__ == "__main__":
-    list_background_processes()
+    background_processes = list_background_processes()
+    print("Processes running in the background:")
+    for pid, name in background_processes:
+        print(f"PID: {pid}, Name: {name}")
